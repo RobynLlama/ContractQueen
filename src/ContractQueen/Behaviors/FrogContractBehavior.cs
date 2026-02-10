ab7a8fd2764b14d2e08ea209a48ea76d4eda8e1c
Robyn <rnixon98@gmail.com>
2026-02-09 21:27:08 -0600

    fix: Check frog state OnEnable

--- src/ContractQueen/Behaviors/FrogContractBehavior.cs
+++ src/ContractQueen/Behaviors/FrogContractBehavior.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using YAPYAP;
 
@@ -17,9 +18,39 @@ public class FrogContractBehavior : MonoBehaviour
       ContractQueenPlugin.Log.LogError("Unable to establish owner in FrogContractBehavior");
       return;
     }
+  }
+
+  private IEnumerator DelayedFrogCheck()
+  {
+    yield return new WaitForEndOfFrame();
+
+    ContractQueenPlugin.Log.LogDebug($"Check valid status for a frog: {gameObject.GetInstanceID()} | State | {Owner.CurrentState.state}");
 
     if (gameObject.name == "NpcFrog (1)" || gameObject.name == "NpcFrog (2)")
+    {
+      ContractQueenPlugin.Log.LogDebug($"Invalidating a unique frog: {gameObject.name}");
       Count();
+      yield break;
+    }
+
+    if (GameManager.Instance.currentGameState != GameManager.GameState.Lobby)
+      yield break;
+
+    //The other two states are `BeingHeld` and `InInventory` so those are both
+    //valid ways to bring a frog home
+    if (Owner.CurrentState.state == PropState.Idle)
+    {
+      ContractQueenPlugin.Log.LogDebug($"Invalidating an IDLE frog: {Owner.GetInstanceID()}");
+      Count();
+    }
+  }
+
+  private void OnEnable()
+  {
+    if (HasBeenCounted)
+      return;
+
+    StartCoroutine(DelayedFrogCheck());
   }
 
   public void Count()
