7215ef74d34463a3d4bd8b92c155c613c84bbcf6
Robyn <rnixon98@gmail.com>
2026-02-16 06:24:22 -0600

    feat: match MUDs to DTOs on frog awake

--- src/ContractQueen/Patches/FrogStateMachine.cs
+++ src/ContractQueen/Patches/FrogStateMachine.cs
@@ -1,4 +1,6 @@
 using ContractQueen.Behaviors;
+using ContractQueen.Persist;
+using FrogDataLib.DataManagement;
 using HarmonyLib;
 using Unity.VisualScripting;
 using YAPYAP.Npc.Frog;
@@ -8,6 +10,16 @@ namespace ContractQueen.Patches;
 internal static class FrogStateMachinePatches
 {
   [HarmonyPatch(typeof(FrogStateMachine), "Awake"), HarmonyPostfix]
-  internal static void AwakePatch(FrogStateMachine __instance) =>
-    __instance.AddComponent<FrogContractBehavior>();
+  internal static void AwakePatch(FrogStateMachine __instance)
+  {
+    //Ignore the pre-placed frogs
+    if (__instance.gameObject.GetInstanceID() > 0)
+      return;
+
+    var comp = __instance.AddComponent<FrogContractBehavior>();
+    AssetMUD token = new(__instance);
+
+    var dto = FrogDatabase.Database.GetFrogData(token);
+    comp.Data = dto;
+  }
 }
